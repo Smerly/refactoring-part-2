5f4af2119984d94d5c7f2dee3c21cbb64b2d8c0d
Ernest Cheung <ernestcheung02@gmail.com>
2022-02-15 22:47:46 -0800

    finished 7-10

---
+++ calculate_area.py
@@ -0,0 +1,35 @@
+# by Kami Bigdely
+# Rename Method
+# Reference: https://parade.com/1039985/marynliles/pick-up-lines/
+
+# TODO: Rename this function to reflect what it's doing.
+def calculate_under_graph(graph):
+    """Calculate the area under the input graph."""
+    # bla bla bla.
+    pass
+
+#####################
+
+
+# TODO: Rename this function to reflect what it's doing.
+def get_largest_value(li):
+    m = li[0]
+    for value in li:
+        if value > m:
+            m = value
+    return m
+
+
+li = [5, -1, 43, 32, 87, -100]
+print(get_largest_value(li))
+
+############################
+
+
+# TODO: Rename this function to reflect what it's doing.
+def arrayify(sentence):
+    words = sentence[0:].split(' ')
+    return words
+
+
+print(arrayify('If you were a vegetable, you’d be a ‘cute-cumber.'))
